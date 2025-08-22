bf79d56abfd246b804513231c4dd9bc9545b578a
REVANTH VANGAPANDU <157471888+pandu200524@users.noreply.github.com>
2025-08-22 18:38:04 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,6 @@
 import foody_logo from './foody_logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
@@ -9,7 +8,6 @@ export const assets ={
     foody_logo,
     add_icon,
     order_icon,
-    profile_image,
     upload_area,
     parcel_icon
 }
